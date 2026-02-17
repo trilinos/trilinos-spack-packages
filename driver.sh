a6cc8bf1ff100d173bdf2ccb2169c04bd234c9be
Joe Frye <jfrye@sandia.gov>
2026-02-17 11:52:01 -0700

    update driver script

--- driver.sh
+++ driver.sh
@@ -1,1 +1,1 @@
-python3 ./spack_packages_from_tribits_xml.py -i ./Trilinos17_package_dependencies.xml
+python3 ./spack_packages_from_tribits_xml.py -i ./xml_files/Trilinos17_package_dependencies.xml
