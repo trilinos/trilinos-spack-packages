80f899fe2ae79bb503265a9783431e51f7211c99
Joe Frye <jfrye@sandia.gov>
2026-08-13 09:04:39 -0600

    cdash integration

--- CTestScript.cmake
+++ CTestScript.cmake
@@ -2,7 +2,7 @@
 # This script is used by docker-cdash.sh
 
 # Set SSL verification to off for corporate environments
-set(CTEST_CURL_OPTIONS "CURLOPT_SSL_VERIFYPEER=0;CURLOPT_SSL_VERIFYHOST=0")
+set(CTEST_CURL_OPTIONS "CURLOPT_SSL_VERIFYPEER;0;CURLOPT_SSL_VERIFYHOST;0")
 
 # Get dashboard type from environment (default: Experimental)
 set(DASHBOARD_TYPE "$ENV{DASHBOARD_TYPE}")
@@ -13,6 +13,9 @@ endif()
 # Set source and binary directories
 set(CTEST_SOURCE_DIRECTORY "/opt/trilinos-spack-packages")
 set(CTEST_BINARY_DIRECTORY "/opt/trilinos-spack-packages/build")
+
+# Set CMake generator
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 # Read configuration from CTestConfig.cmake
 ctest_read_custom_files(${CTEST_SOURCE_DIRECTORY})
